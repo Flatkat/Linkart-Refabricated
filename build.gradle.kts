45c1baac0075ced3bb46dab8165e023708a79a10
rosellle <eliz.urq@gmail.com>
2025-12-20 04:41:40 +1300

    chore: update to new gradle kts, update loom, update gradle to 9.2

---
+++ build.gradle.kts
@@ -0,0 +1,85 @@
+plugins {
+    id("fabric-loom")
+
+    // `maven-publish`
+    // id("me.modmuss50.mod-publish-plugin")
+}
+
+version = "${property("mod.version")}+${stonecutter.current.version}"
+base.archivesName = property("mod.id") as String
+
+val requiredJava = when {
+    stonecutter.eval(stonecutter.current.version, ">=26.1") -> JavaVersion.VERSION_25
+    stonecutter.eval(stonecutter.current.version, ">=1.20.6") -> JavaVersion.VERSION_21
+    stonecutter.eval(stonecutter.current.version, ">=1.18") -> JavaVersion.VERSION_17
+    stonecutter.eval(stonecutter.current.version, ">=1.17") -> JavaVersion.VERSION_16
+    else -> JavaVersion.VERSION_1_8
+}
+
+repositories {
+    // MidnightLib
+    maven("https://maven.midnightdust.eu/releases") { name = "MidnightDust" }
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:${stonecutter.current.version}")
+    mappings("net.fabricmc:yarn:${property("deps.yarn")}:v2")
+    modImplementation("net.fabricmc:fabric-loader:${property("deps.fabric_loader")}")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${property("deps.fabric_api")}")
+
+    val midnightlib = "eu.midnightdust:midnightlib:${property("deps.midnightlib")}"
+    modImplementation(midnightlib) { isTransitive = false }
+    include(midnightlib)
+}
+
+loom {
+    fabricModJsonPath = rootProject.file("src/main/resources/fabric.mod.json") // Useful for interface injection
+//  accessWidenerPath = rootProject.file("src/main/resources/template.accesswidener")
+
+    decompilerOptions.named("vineflower") {
+        options.put("mark-corresponding-synthetics", "1") // Adds names to lambdas - useful for mixins
+    }
+
+    runConfigs.all {
+        ideConfigGenerated(true)
+        vmArgs("-Dmixin.debug.export=true") // Exports transformed classes for debugging
+        runDir = "../../run" // Shares the run directory between versions
+    }
+}
+
+
+java {
+    withSourcesJar()
+    targetCompatibility = requiredJava
+    sourceCompatibility = requiredJava
+}
+
+tasks {
+    processResources {
+        inputs.property("id", project.property("mod.id"))
+        inputs.property("name", project.property("mod.name"))
+        inputs.property("version", project.property("mod.version"))
+        inputs.property("minecraft", project.property("mod.mc_dep"))
+        inputs.property("loader_version", project.property("deps.fabric_loader"))
+
+        val props = mapOf(
+            "id" to project.property("mod.id"),
+            "name" to project.property("mod.name"),
+            "version" to project.property("mod.version"),
+            "minecraft" to project.property("mod.mc_dep"),
+            "loader_version" to project.property("deps.fabric_loader")
+        )
+
+        filesMatching("fabric.mod.json") { expand(props) }
+
+        val mixinJava = "JAVA_${requiredJava.majorVersion}"
+        filesMatching("*.mixins.json") { expand("java" to mixinJava) }
+    }
+
+    register<Copy>("buildAndCollect") {
+        group = "build"
+        from(remapJar.map { it.archiveFile }, remapSourcesJar.map { it.archiveFile })
+        into(rootProject.layout.buildDirectory.file("libs/${project.property("mod.version")}"))
+        dependsOn("build")
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        maven("https://maven.fabricmc.net/") { name = "Fabric" }
+        maven("https://maven.kikugie.dev/snapshots") { name = "KikuGie Snapshots" }
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+  id("dev.kikugie.stonecutter") version "0.7.11"
+}
+
+stonecutter {
+  create(rootProject) {
+    versions("1.21.1", "1.21.4", "1.21.5", "1.21.6", "1.21.7", "1.21.8", "1.21.9", "1.21.10", "1.21.11")
+    vcsVersion = "1.21.1"
+  }
+}
+
+rootProject.name = "linkart-refabricated"
